8cfb34010b0ecec69a1ddf7ea27db5b5114ff0cb
Vardhman-jain <77984872+Vardhman-jain@users.noreply.github.com>
2021-02-03 19:51:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function changecolor1(){
+  document.getElementById("section1").style.color="orangered";
+}
+function changecolor2(){
+  document.getElementById("span1").style.color="whitesmoke";
+}
+function changecolor3(){
+  document.getElementById("span2").style.color="#0000FF";
+}
+function changecolor4(){
+  document.getElementById("span3").style.color="whitesmoke";
+}
+function changecolor5(){
+  document.getElementById("div4").style.color="green";
+}
+function changecolor6(){
+  document.getElementById("h2").style.color="whitesmoke";
+}
